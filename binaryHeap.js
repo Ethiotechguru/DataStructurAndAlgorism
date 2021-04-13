f90fd0360dedf2931d6f2ae128674929c6baf686
Samuel Belete <samyethio891@gmail.com>
2021-04-13 05:38:08 -0700

    improved Max binary heap implementaion

--- binaryHeap.js
+++ binaryHeap.js
@@ -1,62 +1,101 @@
-//In binary heap
+//Implementing Parent Child relationship in an array
+// function PCR(arr){
+// //     let arr = [2,3,5,6,7,8];
+
+//     for(let i in arr){
+//         let left = Math.floor((2*i)+1);
+//         let right = Math.floor((2*i)+2);
+        
+//         if(left>=arr.length){
+//             return 'DONE'
+//         }else{
+//             if(arr[right] && arr[left]){
+//                 console.log('parent', arr[i])
+//                 console.log('children', arr[left], arr[right])
+//             }else{
+//                 console.log('parent', arr[i])
+//                 console.log('child', arr[left])
+//             }
+//         }
+//     }
+// }
+// PCR([10,15,17,19,11,34,54,65,67,70,80]);
+// //parent 10
+// //children 15,17
+// //parent 15
+// //children 19,11
+// //parent 17
+// //children 34,54
+// //parent 19
+// //children 65,67
+// //parent 11
+// //children 70,80
+// //DONE
+
+class MaxBinaryHeap{
+    //In binary heap
     //->Max binary heap is where all child nodes are less than it's parent
     //-> Min binary heap is where all chile nodes are Greater than its parent
-    
-class BinaryHeap{
     constructor(){
         this.list = [];
     }
+
     add(val){
         this.list.push(val);
-        if(this.list.length === 1){
-            return this.list;
+        let cIdx = this.list.length-1;
+        let temp;
+        while(cIdx>0){
+            let pIdx = Math.floor((cIdx-1)/2);
+            if(this.list[cIdx]<=this.list[pIdx]) break;
+            temp = this.list[pIdx];
+            this.list[pIdx] = this.list[cIdx];
+            this.list[cIdx] = temp;
+            cIdx = pIdx;
         }
-
-        let cIdex = this.list.length -1;
-        let pIndex = Math.floor((cIdex-1)/2);
-       while(this.list[cIdex]>this.list[pIndex]){
-           let temp = this.list[pIndex];
-           this.list[pIndex] = this.list[cIdex];
-           this.list[cIdex] = temp;  
-           cIdex = pIndex;
-           pIndex = Math.floor((cIdex-1)/2);
-       }
-    //    console.log(this.list[Math.floor((this.list.length-1)/2)]);
-       return this.list;
+        return this.list;
     }
-    extractMax(){
-        this.list.shift();
-        this.list.unshift(this.list.pop());
-        
+
+    removeMax(){
+        let last = this.list.pop();
+        if(this.list.length) this.list[0] = last;
         let pIdx = 0;
-        let cIdxL = (2*pIdx)+1;
-        let cIdxR = (2*pIdx)+2;
-        while(this.list[pIdx]<this.list[cIdxL] || this.list[pIdx]<this.list[cIdxR]){
+        while(true){
+            let lCIdx = (2*pIdx)+1;
+            let rCIdx = lCIdx+1;
             let temp;
-           if(this.list[cIdxL] > this.list[cIdxR]){
-                temp = this.list[cIdxL];
-                this.list[cIdxL] = this.list[pIdx];
-           }else if(this.list[cIdxL] < this.list[cIdxR]){
-               temp = this.list[cIdxR];
-               this.list[cIdxR] = this.list[pIdx];
-           }
-           this.list[pIdx] = temp;
-           pIdx = cIdxL;
-           cIdxL = (2*pIdx)+1;
-           cIdxR = (2*pIdx)+2;
+            if(this.list[rCIdx]>this.list[lCIdx] &&
+                this.list[rCIdx]>this.list[pIdx]
+            ){
+                temp = this.list[pIdx];
+                this.list[pIdx] = this.list[rCIdx];
+                this.list[rCIdx] = temp;
+                pIdx = lCIdx;
+            }
+            else if(this.list[lCIdx]>this.list[pIdx]){
+                temp = this.list[pIdx];
+                this.list[pIdx] = this.list[lCIdx];
+                this.list[lCIdx] = temp;
+                pIdx = lCIdx;
+            }else{
+                break;
+            }
         }
         return this.list;
     }
 }
 
-let heap = new BinaryHeap();
-heap.add(40);
-heap.add(50);
-heap.add(35);
-heap.add(32);
+let heap= new MaxBinaryHeap();
+heap.add(45);
+heap.add(29);
 heap.add(25);
-heap.add(17);
-heap.add(20);
-heap.add(12);
-heap.add(23)
-heap.add(18)
+heap.add(24);
+heap.add(23);
+//[45, 29, 25, 22, 23]
+    //[23,29, 25, 22] swap
+    //[29,23, 25, 22] bubble Down
+        //[22,23, 25] swap
+        //[25,23, 22] bubble Down
+            //[22,23] swap
+            //[23,22] bubble Down
+                //[22] 
+                    //[]
